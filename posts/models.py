c853b94136668c0189cc2db5dc45e47d3029a657
Pablo Embarach <pabloembarach@gmail.com>
2023-03-21 09:54:19 +0000

    added location_filter and image validation

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,19 @@ class Post(models.Model):
     Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
+
+    location_filter_choices = [
+        ('temple_bar', 'Temple Bar'),
+        ('sandymount', 'Sandymount'),
+        ('rathmines', 'Rathmines'),
+        ('portobello', 'Portobello'),
+        ('ballsbridge', 'Ballsbridge'),
+        ('dublin_docklands', 'Dublin Docklands'),
+        ('smithfield', 'Smithfield'),
+        ('cabra', 'Cabra'),
+        ('ashtown', 'Ashtown'),
+        ('phibsborough', 'Phibsborough')
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,6 +27,9 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
+    location_filter = models.CharField(
+        max_length=32, choices=location_filter_choices, default='none'
     )
 
     class Meta:
