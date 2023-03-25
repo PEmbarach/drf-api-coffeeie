581f4e150cc07b0cb440b772cebea6cc4c1788a6
Pablo Embarach <pabloembarach@gmail.com>
2023-03-25 17:24:52 +0000

    Update models and serializers on posts.

--- posts/serializers.py
+++ posts/serializers.py
@@ -43,6 +43,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image',
+            'image', 'title', 'price', 'rate', 'location',
             'like_id', 'likes_count', 'comments_count',
         ]
